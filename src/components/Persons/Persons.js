4d5ece5d7d426d74a3fc9fa411d73145f9cbfed8
Xavi <xaviermaldonadony@hotmail.com>
2020-12-07 04:49:35 -0500

    Example of component lifecycle creting and update

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+
+class Persons extends Component {
+	// static getDerivedStateFromProps(props, state) {
+	// 	console.log('[Persons.js] getDerivedStateFromProps');
+	// 	return state;
+	// }
+
+	// componentWillReceiveProps(props) {
+	// 	console.log('[Persons.js] componentWillReceiveProps', props);
+	// }
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('[Persons.js] shouldComponentUpdate');
+
+		// returns a boolean
+		return true;
+	}
+
+	getSnapshotBeforeUpdate(prevProps, prevState) {
+		console.log('[Persons.js] getSnapshotBeforeUpdate');
+		return { message: 'Snapshot' };
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		console.log('[Persons.js] componentDidUpdate');
+		console.log(snapshot);
+	}
+
+	render() {
+		console.log('[Persons.js] rendering...');
+		return this.props.persons.map((person, index) => {
+			return (
+				<Person
+					key={person.id}
+					click={() => this.props.clicked(index)}
+					name={person.name}
+					age={person.age}
+					changed={(event) => this.props.changed(event, person.id)}
+				/>
+			);
+		});
+	}
+}
+
+export default Persons;
---
+++ src/containers/App.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import classes from './App.module.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/cockpit/Cockpit';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		console.log('[App.js] constructor');
+		// equal as below
+		// this.state = {
+		// persons: [
+		// 	{ id: 'asfa1', name: 'Max', age: 28 },
+		// 	{ id: 'vasdf1', name: 'Manu', age: 29 },
+		// 	{ id: 'asdf11', name: 'Stephanie', age: 26 },
+		// ],
+		// otherState: 'some other value',
+		// showPersons: false,
+		// }
+	}
+
+	// equal as above modern synstax
+	state = {
+		persons: [
+			{ id: 'asfa1', name: 'Max', age: 28 },
+			{ id: 'vasdf1', name: 'Manu', age: 29 },
+			{ id: 'asdf11', name: 'Stephanie', age: 26 },
+		],
+		otherState: 'some other value',
+		showPersons: false,
+	};
+
+	// when ever your props change or ur class base component u can sync ur state to them
+	// rare
+	// Creationg LC
+	// Update LC
+	static getDerivedStateFromProps(props, state) {
+		console.log('[App.js] getDerivedStateFrom props', props);
+		return state;
+	}
+
+	// componentWillMount() {
+	// 	console.log('[App.js] componentWillMount');
+	// }
+
+	componentDidMount() {
+		console.log('[App.js] componentDidMount');
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('[App.js] shouldComponentUpdate');
+		return true;
+	}
+
+	componentDidUpdate() {
+		console.log('[App.js] componentDidUpdate');
+	}
+
+	nameChangedHandler = (event, id) => {
+		const personIndex = this.state.persons.findIndex((p) => {
+			return p.id === id;
+		});
+
+		const person = {
+			...this.state.persons[personIndex],
+		};
+
+		// const person = Object.assign({}, this.state.persons[personIndex]);
+
+		person.name = event.target.value;
+
+		const persons = [...this.state.persons];
+		persons[personIndex] = person;
+
+		this.setState({ persons: persons });
+	};
+
+	deletePersonHandler = (personIndex) => {
+		// const persons = this.state.persons.slice();
+		const persons = [...this.state.persons];
+		persons.splice(personIndex, 1);
+		this.setState({ persons: persons });
+	};
+
+	togglePersonsHandler = () => {
+		const doesShow = this.state.showPersons;
+		this.setState({ showPersons: !doesShow });
+	};
+
+	render() {
+		console.log('[App.js] render ');
+		let persons = null;
+
+		if (this.state.showPersons) {
+			persons = (
+				<Persons
+					persons={this.state.persons}
+					clicked={this.deletePersonHandler}
+					changed={this.nameChangedHandler}
+				/>
+			);
+		}
+
+		return (
+			<div className={classes.App}>
+				<Cockpit
+					title={this.props.appTitle}
+					showPersons={this.state.showPersons}
+					persons={this.state.persons}
+					clicked={this.togglePersonsHandler}
+				/>
+				{persons}
+			</div>
+		);
+		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+	}
+}
+
+export default App;
+
+// 7 9
